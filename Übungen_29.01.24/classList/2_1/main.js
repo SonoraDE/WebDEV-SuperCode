2b0e056918b129c5e671dd1313bfbb61aa011a53
SonoraDE <tobiastischer@outlook.de>
2024-01-29 16:41:07 +0100

    finish

--- Übungen_29.01.24/classList/2_1/main.js
+++ Übungen_29.01.24/classList/2_1/main.js
@@ -1,4 +1,9 @@
 function styleElements() {
-    document.querySelector("headline")
-    document.querySelector("items")
+    document.querySelector("#bucket").classList.add("headline");
+    document.querySelector("#list").classList.add("items");
+}
+
+function resetElements() {
+    document.querySelector("#bucket").classList.remove("headline");
+    document.querySelector("#list").classList.remove("items");
 }
